967233922ee1358c2f2e4ac7c43ba82735ecadac
landoyjx <landoyjx@163.com>
2020-05-25 14:12:03 +0800

    add automatic testnet config generate

--- generate-testnet.sh
+++ generate-testnet.sh
@@ -7,6 +7,7 @@ emintcli config indent true
 emintcli config trust-node true
 
 # 2. init
+rm -rf testnet/*
 emintd init node0 --chain-id 8 --home testnet/node0
 emintd init node1 --chain-id 8 --home testnet/node1
 emintd init node2 --chain-id 8 --home testnet/node2
@@ -29,14 +30,22 @@ emintd gentx --name mykey1 --home testnet/node1 --ip 192.168.20.3 --node-id $(em
 emintd gentx --name mykey2 --home testnet/node2 --ip 192.168.20.4 --node-id $(emintd tendermint show-node-id --home testnet/node2) --keyring-backend test
 
 # 6. collect-gentxs to genesis.json
+cp testnet/node1/config/gentx/* testnet/node0/config/gentx/
+cp testnet/node2/config/gentx/* testnet/node0/config/gentx/
 emintd collect-gentxs --home testnet/node0
 emintd collect-gentxs --home testnet/node1
 emintd collect-gentxs --home testnet/node2
 
 # 7. collect node1 and node2 genesis.json gentxs, copy to node0 genesis.json gentxs, copy node0 genesis.json to replace others
+rm -f testnet/node1/config/genesis.json
+rm -f testnet/node2/config/genesis.json
+cp testnet/node0/config/genesis.json testnet/node1/config/
+cp testnet/node0/config/genesis.json testnet/node2/config/
 
 # 8. config each node's config.toml persistent_peers to the other two node's node-id@node-ip:26656
+peers=`emintd tendermint show-node-id --home testnet/node0`@192.168.20.2,`emintd tendermint show-node-id --home testnet/node1`@192.168.20.3,`emintd tendermint show-node-id --home testnet/node2`@192.168.20.4
+gsed -i '175,175d' testnet/node0/config/config.toml
+gsed -i "174a persistent_peers = \"$peers\"" testnet/node0/config/config.toml
 
-# 9. copy each node config to docker instance
-
-# A. start each node, emintd start --home node* --rpc.unsafe --log_level "main:info,state:info,mempool:info"
+# 9. start each node, emintd start --home node* --rpc.unsafe --log_level "main:info,state:info,mempool:info"
+echo -e "\n------Enjoy it!------"
